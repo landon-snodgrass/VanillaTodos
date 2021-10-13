787edba16b15ae1475827a28667faf80f172cc69
Landon Snodgrass <g.l.snodgrass95@gmail.com>
2021-10-13 15:05:19 -0700

    Added some styling

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let todoList;
 
 class Todo {
     constructor(todoText, id) {
-        this.template = `<li class="todo-item" id="todo-item-${id}"><a href="#" class="todo-check" id="todo-check-${id}" data-id="${id}"><i class="far fa-circle todo-item-checkbox" id="todo-item-checkbox-${id}"></i></a><span class="todo-item-text">${todoText}</span></li>`;
+        this.template = `<li class="todo-item" id="todo-item-${id}"><a href="#" class="todo-check text-danger" id="todo-check-${id}" data-id="${id}"><i class="far fa-circle todo-item-checkbox" id="todo-item-checkbox-${id}"></i></a><span class="todo-item-text">${todoText}</span></li>`;
         this.completed = false;
         this.text = todoText;
         this.id = id
