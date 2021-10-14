d06cedba3878067f386ab407cb35c1a08eb6c527
Landon Snodgrass <g.l.snodgrass95@gmail.com>
2021-10-14 10:15:23 -0700

    Edit and delete now work

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let todoList;
 
 class Todo {
     constructor(todoText, id) {
-        this.template = `<li class="todo-item" id="todo-item-${id}"><a href="#" class="todo-check text-danger" id="todo-check-${id}" data-id="${id}"><i class="far fa-circle todo-item-checkbox" id="todo-item-checkbox-${id}"></i></a><span class="todo-item-text">${todoText}</span></li>`;
+        this.template = `<li class="todo-item" id="todo-item-${id}"><a href="#" class="todo-check text-danger" id="todo-check-${id}" data-id="${id}"><i class="far fa-circle todo-item-checkbox" id="todo-item-checkbox-${id}"></i></a><span class="todo-item-text">${todoText}</span><span class="controls"><a href="#" class="delete-todo" data-id="${id}""><i class="fas fa-trash"></i></a><a href="#" class="edit-todo" data-id="${id}"><i class="fas fa-edit"></i></a></span></li>`;
         this.completed = false;
         this.text = todoText;
         this.id = id
@@ -30,9 +30,24 @@ class TodoList {
         this.todoList = {};
         this.element = document.getElementById("todo-list");
         this.currentIndex = 0;
+
+        // Event delegation
         this.element.addEventListener("click", (event) => {
+            // Check/Uncheck todo item
             if (event.target.parentElement && event.target.parentElement.matches("a.todo-check")) {
                 this.todoList[event.target.parentElement.dataset.id].checkUncheck();
+            }
+
+            // Delete todo item
+            if (event.target.parentElement && event.target.parentElement.matches("a.delete-todo")) {
+                const todoId = event.target.parentElement.dataset.id;
+                this.deleteTodo(todoId);
+            }
+
+            // Edit todo item
+            if (event.target.parentElement && event.target.parentElement.matches("a.edit-todo")) {
+                const todoId = event.target.parentElement.dataset.id;
+                this.editTodo(todoId);
             }
         })
     }
@@ -44,6 +59,16 @@ class TodoList {
         this.currentIndex++;
 
         this.element.innerHTML += newTodo.template;
+    }
+
+    deleteTodo(todoId) {
+        this.todoList[todoId].getTodoElement().remove();
+        delete this.todoList[todoId];
+    }
+
+    editTodo(todoId) {
+        document.getElementById("todo-text").value = this.todoList[todoId].text;
+        document.getElementById("add-todo-button").setAttribute("edit", todoId);
     }
 }
 
@@ -62,11 +87,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const addTodoClick = (event) => {
+    const edit = event.target.getAttribute("edit");
     const todoText = document.getElementById("todo-text").value;
-    console.log(todoText);
+
+    console.log(edit);
+
     if (todoText == "") {
         console.log("Error");
         return;
+    }
+
+    if (edit && edit != "") {
+        todoList.deleteTodo(edit)
     }
 
     todoList.addTodo(todoText);
